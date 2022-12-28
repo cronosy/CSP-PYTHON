6f30582cdcaaef47807b6771e42cdd85b5fcd8da
luis.prieto <luis.prieto@lla.com>
2022-12-27 19:20:08 -0500

    add setup python

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='csp_connect',
+version='1',
+description='installation of csp_connect',
+url='#',
+author='cronosy',
+author_email='cronosyaghar@gmail.com',
+license='MIT',
+packages=['csp_connect'],
+zip_safe=False)
